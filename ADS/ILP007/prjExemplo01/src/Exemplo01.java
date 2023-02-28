19378db45b268f22b0662a7488334de1f31b3faa
evandro-rtib <111821224+evandro-rtib@users.noreply.github.com>
2023-02-28 09:44:31 -0300

    Add files via upload

---
+++ ADS/ILP007/prjExemplo01/src/Exemplo01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author 0030481921018
+ */
+public class Exemplo01 {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+       int codProd,qtdeVend;
+       String descProd;
+       double precUnit, valCompra;
+       
+       System.out.print("Digite o código do produto: ");
+       
+    }//FIM DO MAIN
+}//FIM DA CLASSE
